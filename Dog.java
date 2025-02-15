698e7232b8cb1940a2f1c60f37dcca33d6f3b8da
htkimhue <thihuekimhuynh@gmail.com>
2025-02-15 23:13:29 +0700

    Add files via upload

---
+++ Dog.java
@@ -0,0 +1,18 @@
+package Assignment11.Exercise2;
+
+import java.util.Date;
+
+public class Dog extends Animal {
+
+    String breed;
+
+    public Dog(String id, String source, Date dateOfBirth, String color) {
+        super(id, source, dateOfBirth, color);
+        this.breed = breed;
+    }
+
+    public void makeSound() {
+        System.out.println("Gau Gau");
+    }
+
+}
